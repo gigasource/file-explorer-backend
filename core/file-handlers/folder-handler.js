5195578be9ad76547a5f8f7be6277cd3bd635601
ThinhVu <vutrongthinhk7@gmail.com>
2023-04-30 15:45:01 +0700

    Improve constructFolderTree performance

--- core/file-handlers/folder-handler.js
+++ core/file-handlers/folder-handler.js
@@ -48,104 +48,34 @@ async function listFilesByFolder(folderPath, namespace) {
   }));
 }
 
-async function listFoldersByFolder(folderPath, namespace) {
-  if (!folderPath.endsWith('/')) folderPath += '/';
-
-  return getFileMetadataStorage().findFileMetadatas(transformExternal(
-      {
-        folderPath,
-        isFolder: true,
-        ...(namespace ? {namespace} : {}),
-      }
-  ));
-}
-
 async function constructFolderTree(namespace) {
-  const foldersAtRoot = await listFoldersByFolder('/', namespace);
-
-  async function traverseFolderList(folderList) {
-    const list = [];
-
-    for (const f of folderList) {
-      const folderName = f.fileName;
-      const folderPath = f.folderPath;
-      const childrenFolders = await listFoldersByFolder(folderPath + folderName, namespace);
-      const folders = await traverseFolderList(childrenFolders);
-
-      list.push({
-        folderName, folderPath, folders,
-      });
+  const maps = {}
+  const folders = await getFileMetadataStorage().findFileMetadatas(transformInternal({
+    isFolder: true,
+    ...(namespace ? {namespace} : {})
+  }))
+  for (const folder of folders) {
+    const myPath = `${folder.folderPath}${folder.fileName}/`;
+    if (!maps[myPath]) {
+      maps[myPath] = {
+        folderName: folder.fileName,
+        folderPath: folder.folderPath,
+        folders: []
+      }
     }
-
-    return list.sort((a, b) => a.folderName > b.folderName);
+    const myNode = maps[myPath]
+    if (!maps[folder.folderPath]) {
+      maps[folder.folderPath] = {
+        folderName: folder.fileName,
+        folderPath: folder.folderPath,
+        folders: []
+      }
+    }
+    const parentNode = maps[folder.folderPath]
+    parentNode.folders.push(myNode)
   }
-
-  return [{
-    folderName: '/',
-    folders: await traverseFolderList(transformInternal(foldersAtRoot))
-  }]
+  return [maps['/']]
 }
-
-/* Keep the code for future use (append folder tree instead of generating the whole tree)
-async function constructFolderTree2(folderPath, namespace) {
-  if (!folderPath.endsWith('/')) folderPath += '/';
-
-  const folderArray = [];
-  const treePathIdentifier = 'folders';
-  // let selectedTreePath = `${treePathIdentifier}.0`;
-  let selectedFolder;
-
-  const paths = folderPath.split('/');
-  paths.pop();
-  const currentPathArray = [];
-
-  while (paths.length > 0) {
-    currentPathArray.push(paths.shift());
-    let currentPath = currentPathArray.join('/');
-    if (!currentPath.endsWith('/')) currentPath += '/';
-
-    let folders = await listFilesByFolder(currentPath, namespace);
-    folders = folders.map((f, index) => {
-      const folderNames = f.folderPath.split('/');
-      const selected = f.folderPath + f.fileName + '/' === folderPath
-
-      const folderInfo = {
-        folderName: f.fileName,
-        folderPath: f.folderPath,
-        parentFolderName: f.folderPath === '/' ? '/' : folderNames[folderNames.length - 2],
-        treePath: `${treePathIdentifier}.${index}`,
-        ...(selected ? {selected} : {}),
-      };
-
-      if (selected) selectedFolder = folderInfo;
-
-      return folderInfo;
-    })
-
-    folderArray.push(folders);
-  }
-
-  while (folderArray.length > 1) {
-    let folders = folderArray.pop() || [];
-    if (folders.length === 0) continue;
-
-    const parentFolder = folderArray[folderArray.length - 1].find(f => f.folderName === folders[0].parentFolderName);
-    folders = folders.forEach(f => {
-    })
-    parentFolder.folders = folders;
-  }
-
-  return {
-    folderTree: [{
-      folderName: '/',
-      treePath: `${treePathIdentifier}.0`,
-      folders: folderArray[0],
-    }],
-    selectedFolder,
-    // selectedTreePath,
-  };
-}
-*/
 
 module.exports = {
   createFolder,
